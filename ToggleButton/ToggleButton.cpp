0919e1a4ce6fdfb942f54bd380118e5842a8b84a
Dieter Niklaus <dieter.niklaus@gmx.net>
2015-10-09 17:49:44 +0200

    Added adapter with notifyStatusChanged() facility; made button polarity configurable.
    Signed-off-by: Dieter Niklaus <dieter.niklaus@gmx.net>

--- ToggleButton/ToggleButton.cpp
+++ ToggleButton/ToggleButton.cpp
@@ -40,14 +40,19 @@ public:
 
 //-----------------------------------------------------------------------------
 
-ToggleButton::ToggleButton(int buttonPin, int indicatorPin)
+const bool ToggleButton::IS_POS_LOGIC = false;
+const bool ToggleButton::IS_NEG_LOGIC = true;
+
+ToggleButton::ToggleButton(int buttonPin, int indicatorPin, bool isButtonNegativeLogic, ToggleButtonAdapter* adapter)
 : m_debounceTimer(new Timer(new MyDebounceTimerAdatper(this), Timer::IS_RECURRING, s_defaultKeyPollTime))
+, m_adapter(adapter)
+, m_isButtonNegativeLogic(isButtonNegativeLogic)
 , m_isActive(false)
 , m_buttonPin(buttonPin)
 , m_indicatorPin(indicatorPin)
 { 
   pinMode(m_buttonPin, INPUT);
-  digitalWrite(m_buttonPin, LOW); // pull-down
+  digitalWrite(m_buttonPin, m_isButtonNegativeLogic ? HIGH : LOW); // pull
 
   pinMode(m_indicatorPin, OUTPUT);
   digitalWrite(m_indicatorPin, m_isActive);
@@ -59,6 +64,16 @@ ToggleButton::~ToggleButton()
   delete m_debounceTimer; m_debounceTimer = 0;
 }
 
+ToggleButtonAdapter* ToggleButton::adapter()
+{
+  return m_adapter;
+}
+
+void ToggleButton::attachAdapter(ToggleButtonAdapter* adapter)
+{
+  m_adapter = adapter;
+}
+
 bool ToggleButton::isActive()
 {
   return m_isActive;
@@ -66,18 +81,28 @@ bool ToggleButton::isActive()
 
 void ToggleButton::setIsActive(bool isActive)
 {
+  bool changed = (isActive != m_isActive);
   m_isActive = isActive;
   digitalWrite(m_indicatorPin, m_isActive);
+  if ((0 != m_adapter) && (changed))
+  {
+    m_adapter->notifyStatusChanged(m_isActive);
+  }
 }
 
 void ToggleButton::toggle()
 {
   m_isActive = !m_isActive;
   digitalWrite(m_indicatorPin, m_isActive);
+  if (0 != m_adapter)
+  {
+    m_adapter->notifyStatusChanged(m_isActive);
+  }
 }
 
 bool ToggleButton::isButtonPressed()
 {
-  return (digitalRead(m_buttonPin));
+  bool pressed = digitalRead(m_buttonPin);
+  return (m_isButtonNegativeLogic ? !pressed : pressed);
 }
 
--- ToggleButton/ToggleButton.h
+++ ToggleButton/ToggleButton.h
@@ -3,6 +3,14 @@
 
 class Timer;
 
+//-----------------------------------------------------------------------------
+class ToggleButtonAdapter
+{
+public:
+  virtual void notifyStatusChanged(bool isActive) = 0;
+};
+//-----------------------------------------------------------------------------
+
 class ToggleButton
 {
 public:
@@ -11,15 +19,29 @@ public:
    * @param buttonPin 
    * @param indicatorPin
    */
-  ToggleButton(int buttonPin, int indicatorPin);
+  ToggleButton(int buttonPin, int indicatorPin, bool isButtonNegativeLogic = false, ToggleButtonAdapter* adapter = 0);
   virtual ~ToggleButton();
+  ToggleButtonAdapter* adapter();
+  void attachAdapter(ToggleButtonAdapter* adapter);
   bool isActive();
   void setIsActive(bool isActive);
   void toggle();
   bool isButtonPressed();
 
+  /**
+   * Constant for isButtonNegativeLogic parameter of the constructor (@see ToggleButton()), to create a button switching the pin to HIGH.
+   */
+  static const bool IS_POS_LOGIC;
+
+  /**
+   * Constant for isButtonNegativeLogic parameter of the constructor (@see ToggleButton()), to create a button switching the pin to LOW.
+   */
+  static const bool IS_NEG_LOGIC;
+
 private:
   Timer* m_debounceTimer;
+  ToggleButtonAdapter* m_adapter;
+  bool m_isButtonNegativeLogic;
   bool m_isActive;
   int m_buttonPin;
   int m_indicatorPin;
